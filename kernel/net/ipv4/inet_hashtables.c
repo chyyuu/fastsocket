e7f92074a5c1210509eafca26f3f85a2b51277c2
xiaofeng6 <xiaofeng6@staff.sina.com.cn>
2013-11-25 13:57:39 +0800

    Add Function: 1. RFD module param can enable or disable per-cpu port selection when connect to remote server

--- kernel/net/ipv4/inet_hashtables.c
+++ kernel/net/ipv4/inet_hashtables.c
@@ -639,8 +639,119 @@ void inet_unhash(struct sock *sk)
 }
 EXPORT_SYMBOL_GPL(inet_unhash);
 
-
 int __inet_hash_connect(struct inet_timewait_death_row *death_row,
+		struct sock *sk, u32 port_offset,
+		int (*check_established)(struct inet_timewait_death_row *,
+			struct sock *, __u16, struct inet_timewait_sock **),
+		void (*hash)(struct sock *sk))
+{
+	struct inet_hashinfo *hinfo = death_row->hashinfo;
+	const unsigned short snum = inet_sk(sk)->num;
+	struct inet_bind_hashbucket *head;
+	struct inet_bind_bucket *tb;
+	int ret;
+	struct net *net = sock_net(sk);
+
+	if (!snum) {
+		int i, remaining, low, high, port;
+		static u32 hint;
+		u32 offset = hint + port_offset;
+		struct hlist_node *node;
+		struct inet_timewait_sock *tw = NULL;
+
+		DPRINTK(INFO, "Hint: %u - base offset: %u - new offset: %u\n", 
+			hint, port_offset, offset);
+
+		inet_get_local_port_range(&low, &high);
+		remaining = (high - low) + 1;
+
+		DPRINTK(INFO, "Port pool:%d [%d - %d]\n", remaining, low, high);
+
+		local_bh_disable();
+		for (i = 1; i <= remaining; i++) {
+			port = low + (i + offset) % remaining;
+
+			DPRINTK(INFO, "Target port %d\n", port);
+
+			if (inet_is_reserved_local_port(port))
+				continue;
+			head = &hinfo->bhash[inet_bhashfn(net, port,
+					hinfo->bhash_size)];
+			spin_lock(&head->lock);
+
+			/* Does not bother with rcv_saddr checks,
+			 * because the established check is already
+			 * unique enough.
+			 */
+			inet_bind_bucket_for_each(tb, node, &head->chain) {
+				if (ib_net(tb) == net && tb->port == port) {
+					if (tb->fastreuse >= 0)
+						goto next_port;
+					WARN_ON(hlist_empty(&tb->owners));
+					if (!check_established(death_row, sk,
+								port, &tw))
+						goto ok;
+					goto next_port;
+				}
+			}
+
+			tb = inet_bind_bucket_create(hinfo->bind_bucket_cachep,
+					net, head, port);
+			if (!tb) {
+				spin_unlock(&head->lock);
+				break;
+			}
+			tb->fastreuse = -1;
+			goto ok;
+
+		next_port:
+			spin_unlock(&head->lock);
+		}
+		local_bh_enable();
+
+		return -EADDRNOTAVAIL;
+
+ok:
+		hint += i;
+
+		DPRINTK(INFO, "Port selected:%u - hint updated: %u\n", port, hint);
+
+		/* Head lock still held and bh's disabled */
+		inet_bind_hash(sk, tb, port);
+		if (sk_unhashed(sk)) {
+			inet_sk(sk)->sport = htons(port);
+			hash(sk);
+		}
+		spin_unlock(&head->lock);
+
+		if (tw) {
+			inet_twsk_deschedule(tw, death_row);
+			inet_twsk_put(tw);
+		}
+
+		ret = 0;
+		goto out;
+	}
+
+	head = &hinfo->bhash[inet_bhashfn(net, snum, hinfo->bhash_size)];
+	tb  = inet_csk(sk)->icsk_bind_hash;
+	spin_lock_bh(&head->lock);
+	if (sk_head(&tb->owners) == sk && !sk->sk_bind_node.next) {
+		hash(sk);
+		spin_unlock_bh(&head->lock);
+		return 0;
+	} else {
+		spin_unlock(&head->lock);
+		/* No definite answer... Walk to established hash table */
+		ret = check_established(death_row, sk, snum, NULL);
+out:
+		local_bh_enable();
+		return ret;
+	}
+}
+
+
+int fastsocket_inet_hash_connect(struct inet_timewait_death_row *death_row,
 		struct sock *sk, u32 port_offset,
 		int (*check_established)(struct inet_timewait_death_row *,
 			struct sock *, __u16, struct inet_timewait_sock **),
@@ -788,11 +899,25 @@ out:
 /*
  * Bind a port for a connect operation and hash it.
  */
+
+//XIAOFENG6
+extern int enable_receive_flow_deliver;
+//XIAOFENG6
+
 int inet_hash_connect(struct inet_timewait_death_row *death_row,
 		      struct sock *sk)
 {
-	return __inet_hash_connect(death_row, sk, inet_sk_port_offset(sk),
-			__inet_check_established, __inet_hash_nolisten);
+	//XIAOFENG6
+	int ret; 
+	if (enable_receive_flow_deliver)
+		ret = fastsocket_inet_hash_connect(death_row, sk, inet_sk_port_offset(sk),
+				__inet_check_established, __inet_hash_nolisten);
+	else
+		ret = __inet_hash_connect(death_row, sk, inet_sk_port_offset(sk), 
+				__inet_check_established, __inet_hash_nolisten);
+
+	return ret;
+	//XIAOFENG6
 }
 
 EXPORT_SYMBOL_GPL(inet_hash_connect);
