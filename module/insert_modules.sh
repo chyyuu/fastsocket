b8d8e7a0460f087c55af75a3c0b83d4203232d50
xiaofeng6 <xiaofeng6@staff.sina.com.cn>
2013-12-04 17:01:18 +0800

    Clean Project: 1. Move some test scripts out of repository.

--- module/insert_modules.sh
+++
@@ -1,7 +0,0 @@
-killall haproxy
-killall server
-sleep 1
-
-rmmod fastsocket.ko 
-make clean && make && insmod fastsocket.ko enable_listen_spawn=2 fsocket_debug_level=3 enable_receive_flow_deliver=1
-
