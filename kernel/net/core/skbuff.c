3edc99359d34aea82906e88cd166a269cf5a5d63
xiaofeng6 <xiaofeng6@staff.sina.com.cn>
2014-03-23 20:06:03 +0800

    Fix Bug: 1. Copy skb->peek_sk when clone skb.

--- kernel/net/core/skbuff.c
+++ kernel/net/core/skbuff.c
@@ -548,6 +548,7 @@ static void __copy_skb_header(struct sk_buff *new, const struct sk_buff *old)
 	new->mac_header		= old->mac_header;
 	skb_dst_set(new, dst_clone(skb_dst(old)));
 	new->sock_dst		= old->sock_dst;
+	new->peek_sk		= old->peek_sk;
 	new->rxhash		= old->rxhash;
 #ifdef CONFIG_XFRM
 	new->sp			= secpath_get(old->sp);
